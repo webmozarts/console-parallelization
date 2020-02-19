b2d3177247334b000a426f43530c1f21f298a868
Nicolas Eeckeloo <neeckeloo@gmail.com>
2020-02-19 19:51:33 +0100

    Some fixes

--- src/ContainerAwareCommand.php
+++ src/ContainerAwareCommand.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Webmozarts\Console\Parallelization;
 
+use LogicException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -24,17 +25,12 @@ abstract class ContainerAwareCommand extends Command implements ContainerAwareIn
      */
     private $container;
 
-    /**
-     * @return ContainerInterface
-     *
-     * @throws \LogicException
-     */
-    protected function getContainer()
+    protected function getContainer(): ContainerInterface
     {
         if (null === $this->container) {
             $application = $this->getApplication();
             if (null === $application) {
-                throw new \LogicException('The container cannot be retrieved as the application instance is not yet set.');
+                throw new LogicException('The container cannot be retrieved as the application instance is not yet set.');
             }
 
             $this->container = $application->getKernel()->getContainer();
@@ -43,10 +39,7 @@ abstract class ContainerAwareCommand extends Command implements ContainerAwareIn
         return $this->container;
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function setContainer(ContainerInterface $container = null)
+    public function setContainer(ContainerInterface $container = null): void
     {
         $this->container = $container;
     }
