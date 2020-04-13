d9d9d9068d2dc1ab4c38afd7f4243c41713ecc8e
Bernhard Schussek <bernhard.schussek@webmozarts.com>
2020-04-13 16:38:01 +0200

    Added support for Symfony 3 (#18)

--- tests/ParallelizationIntegrationTest.php
+++ tests/ParallelizationIntegrationTest.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 namespace Webmozarts\Console\Parallelization;
 
 use PHPUnit\Framework\TestCase;
+use function method_exists;
 use function preg_replace;
 use function str_replace;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
@@ -75,8 +76,25 @@ class ParallelizationIntegrationTest extends TestCase
 
         $actual = $this->getOutput();
 
-        $this->assertSame(
-            <<<'EOF'
+        if ($this->isSymfony3()) {
+            $this->assertSame(
+                <<<'EOF'
+Processing 2 movies in segments of 50, batches of 50, 1 round, 1 batches in 1 process
+
+ 0/2 [>---------------------------]   0% < 1 sec/< 1 sec 10.0 MiB
+ 1/2 [==============>-------------]  50% < 1 sec/< 1 sec 10.0 MiB
+ 2/2 [============================] 100% < 1 sec/< 1 sec 10.0 MiB
+
+Processed 2 movies.
+
+EOF
+                ,
+                $actual,
+                'Expected logs to be identical'
+            );
+        } else {
+            $this->assertSame(
+                <<<'EOF'
 Processing 2 movies in segments of 50, batches of 50, 1 round, 1 batches in 1 process
 
  0/2 [>---------------------------]   0% < 1 sec/< 1 sec 10.0 MiB
@@ -85,10 +103,11 @@ Processing 2 movies in segments of 50, batches of 50, 1 round, 1 batches in 1 pr
 Processed 2 movies.
 
 EOF
-            ,
-            $actual,
-            'Expected logs to be identical'
-        );
+                ,
+                $actual,
+                'Expected logs to be identical'
+            );
+        }
     }
 
     public function test_it_can_run_the_command_with_multiple_processes(): void
@@ -103,8 +122,25 @@ EOF
 
         $actual = $this->getOutput();
 
-        $this->assertSame(
-        <<<'EOF'
+        if ($this->isSymfony3()) {
+            $this->assertSame(
+                <<<'EOF'
+Processing 2 movies in segments of 50, batches of 50, 1 rounds, 1 batches in 2 processes
+
+ 0/2 [>---------------------------]   0% < 1 sec/< 1 sec 10.0 MiB
+ 1/2 [==============>-------------]  50% < 1 sec/< 1 sec 10.0 MiB
+ 2/2 [============================] 100% < 1 sec/< 1 sec 10.0 MiB
+
+Processed 2 movies.
+
+EOF
+                ,
+                $actual,
+                'Expected logs to be identical'
+            );
+        } else {
+            $this->assertSame(
+                <<<'EOF'
 Processing 2 movies in segments of 50, batches of 50, 1 rounds, 1 batches in 2 processes
 
  0/2 [>---------------------------]   0% < 1 sec/< 1 sec 10.0 MiB
@@ -113,10 +149,11 @@ Processing 2 movies in segments of 50, batches of 50, 1 rounds, 1 batches in 2 p
 Processed 2 movies.
 
 EOF
-            ,
-            $actual,
-            'Expected logs to be identical'
-        );
+                ,
+                $actual,
+                'Expected logs to be identical'
+            );
+        }
     }
 
     public function test_it_can_run_the_command_with_one_process_as_child_process(): void
@@ -131,8 +168,25 @@ EOF
 
         $actual = $this->getOutput();
 
-        $this->assertSame(
-            <<<'EOF'
+        if ($this->isSymfony3()) {
+            $this->assertSame(
+                <<<'EOF'
+Processing 2 movies in segments of 50, batches of 50, 1 round, 1 batches in 1 process
+
+ 0/2 [>---------------------------]   0% < 1 sec/< 1 sec 10.0 MiB
+ 1/2 [==============>-------------]  50% < 1 sec/< 1 sec 10.0 MiB
+ 2/2 [============================] 100% < 1 sec/< 1 sec 10.0 MiB
+
+Processed 2 movies.
+
+EOF
+                ,
+                $actual,
+                'Expected logs to be identical'
+            );
+        } else {
+            $this->assertSame(
+                <<<'EOF'
 Processing 2 movies in segments of 50, batches of 50, 1 round, 1 batches in 1 process
 
  0/2 [>---------------------------]   0% < 1 sec/< 1 sec 10.0 MiB
@@ -141,10 +195,11 @@ Processing 2 movies in segments of 50, batches of 50, 1 round, 1 batches in 1 pr
 Processed 2 movies.
 
 EOF
-            ,
-            $actual,
-            'Expected logs to be identical'
-        );
+                ,
+                $actual,
+                'Expected logs to be identical'
+            );
+        }
     }
 
     private function getOutput(): string
@@ -158,5 +213,10 @@ EOF
         );
 
         return str_replace(PHP_EOL, "\n", $output);
+    }
+
+    private function isSymfony3(): bool
+    {
+        return method_exists(Application::class, 'getTerminalDimensions');
     }
 }
