c744f3ce531ae95e11898d712adc1b9cfd00a411
agruenwald <a.gruenw@gmail.com>
2020-06-04 12:32:51 +0200

    Improve documentation.

--- src/Parallelization.php
+++ src/Parallelization.php
@@ -555,7 +555,7 @@ trait Parallelization
     /**
      * Ensure that an option value is quoted correctly, before it is passed to a child process.
      * @param mixed $value the input option value, which is typically a string but can be of any other primitive type.
-     * @return mixed
+     * @return mixed the replaced and quoted value, if $value contained a character that required quoting.
      */
     protected function quoteOptionValue($value) {
 
@@ -568,7 +568,6 @@ trait Parallelization
 
     /**
      * Validate whether a command option requires quoting or not, depending on its content.
-     * @param mixed $value
      */
     protected function isValueRequiresQuoting($value) : bool {
         return 0 < preg_match('/[ \s \\ \' " \: \{ \} \[ \] , & \* \# \? \- ? | < > = ! % @ ` ]/x', $value);
