0cb1e09f22efc7440248f68e081d9e838c62e67f
Théo FIDRY <theo.fidry@webmozarts.com>
2022-10-01 11:50:12 +0200

    Fix

--- src/RawOptionsInput.php
+++ src/RawOptionsInput.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Webmozarts Console Parallelization package.
+ *
+ * (c) Webmozarts GmbH <office@webmozarts.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace Webmozarts\Console\Parallelization;
@@ -14,19 +23,21 @@ use Symfony\Component\Console\Input\InputInterface;
  */
 final class RawOptionsInput extends Input
 {
-    private function __construct(InputDefinition $definition = null)
+    private function __construct(?InputDefinition $definition = null)
     {
         parent::__construct($definition);
     }
 
+    /**
+     * Returns all the given options NOT merged with the default values.
+     *
+     * @return array<string|bool|int|float|null|array<string|bool|int|float|null>>
+     */
     public static function getRawOptions(InputInterface $input): array
     {
-        return $input->options;
-    }
-
-    protected function parse()
-    {
-        throw new DomainException('Not implemented.');
+        return $input instanceof Input
+            ? $input->options
+            : [];
     }
 
     public function getFirstArgument()
@@ -40,6 +51,11 @@ final class RawOptionsInput extends Input
     }
 
     public function getParameterOption($values, $default = false, bool $onlyParams = false)
+    {
+        throw new DomainException('Not implemented.');
+    }
+
+    protected function parse()
     {
         throw new DomainException('Not implemented.');
     }
