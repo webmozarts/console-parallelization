8e0b55f07daab68dcda8d4438f23fd1cfe56f4df
Théo FIDRY <theo.fidry@webmozarts.com>
2022-10-02 22:53:52 +0200

    WIP

---
+++ tests/ParallelExecutorTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webmozarts\Console\Parallelization;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Webmozarts\Console\Parallelization\ParallelExecutor
+ */
+final class ParallelExecutorTest extends TestCase
+{
+    public function test_it_can_execute_a_child_process()
+    {
+        
+    }
+}
