a5700f2a90363b5d986659def3a83d8cd82f460b
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2022-10-15 15:23:29 +0200

    Checks the Symfony different version compatibilies in the CI (#122)
    We desire the package to work from PHP 7.4 to 8.1 with Symfony 4.4, 5.4 and 6.x. However the current configuration of the CI does not test at all Symfony 4.4 & 5.4.
    
    This PR aims to do so by:
    
    - Introducing a new job with a dedicated matrix
    - Enforcing the Symfony versions via conflict rules (since we do not use Flex...)

---
+++ bin/set-composer-conflicts.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the Webmozarts Console Parallelization package.
+ *
+ * (c) Webmozarts GmbH <office@webmozarts.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Webmozarts\Console\Parallelization\CI;
+
+use function count;
+use stdClass;
+
+/**
+ * @param list<non-empty-string> $packageNames
+ * @param non-empty-string|null  $conflict
+ */
+function set_composer_conflicts(
+    stdClass $decodedComposerJson,
+    array $packageNames,
+    ?string $conflict
+): void {
+    if (0 === count($packageNames)) {
+        return;
+    }
+
+    $decodedComposerJson->conflict = $decodedComposerJson->conflict ?? new stdClass();
+
+    if (null === $conflict) {
+        foreach ($packageNames as $packageName) {
+            unset($decodedComposerJson->conflict->$packageName);
+        }
+    } else {
+        foreach ($packageNames as $packageName) {
+            $decodedComposerJson->conflict->$packageName = $conflict;
+        }
+    }
+}
---
+++ tests/SetComposerConflictsTest.php
@@ -0,0 +1,122 @@
+<?php
+
+/*
+ * This file is part of the Webmozarts Console Parallelization package.
+ *
+ * (c) Webmozarts GmbH <office@webmozarts.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Webmozarts\Console\Parallelization;
+
+require_once __DIR__.'/../bin/set-composer-conflicts.php';
+
+use function is_array;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+use function Webmozarts\Console\Parallelization\CI\set_composer_conflicts;
+
+/**
+ * @coversNothing
+ */
+final class SetComposerConflictsTest extends TestCase
+{
+    /**
+     * @dataProvider conflictProvider
+     *
+     * @param list<non-empty-string> $packageNames
+     * @param non-empty-string|null  $conflict
+     */
+    public function test_it_can_the_desired_packages_to_the_composer_conflict_section(
+        stdClass $decodedComposerJson,
+        array $packageNames,
+        ?string $conflict,
+        stdClass $expected
+    ): void {
+        set_composer_conflicts($decodedComposerJson, $packageNames, $conflict);
+
+        self::assertEquals($expected, $decodedComposerJson);
+    }
+
+    public static function conflictProvider(): iterable
+    {
+        yield 'nominal' => [
+            self::toStdClass([
+                'package' => 'webmozarts/console-parallelization',
+            ]),
+            ['symfony/cache', 'symfony/framework-bundle'],
+            '>=5.0',
+            self::toStdClass([
+                'package' => 'webmozarts/console-parallelization',
+                'conflict' => [
+                    'symfony/cache' => '>=5.0',
+                    'symfony/framework-bundle' => '>=5.0',
+                ],
+            ]),
+        ];
+
+        yield 'no packages' => [
+            self::toStdClass([
+                'package' => 'webmozarts/console-parallelization',
+            ]),
+            [],
+            '>=5.0',
+            self::toStdClass([
+                'package' => 'webmozarts/console-parallelization',
+            ]),
+        ];
+
+        yield 'already has a conflict section' => [
+            self::toStdClass([
+                'package' => 'webmozarts/console-parallelization',
+                'conflict' => [
+                    'symfony/service-contracts' => '<2.0',
+                ],
+            ]),
+            ['symfony/cache', 'symfony/framework-bundle'],
+            '>=5.0',
+            self::toStdClass([
+                'package' => 'webmozarts/console-parallelization',
+                'conflict' => [
+                    'symfony/service-contracts' => '<2.0',
+                    'symfony/cache' => '>=5.0',
+                    'symfony/framework-bundle' => '>=5.0',
+                ],
+            ]),
+        ];
+
+        yield 'removes conflicts if no conflict is provided' => [
+            self::toStdClass([
+                'package' => 'webmozarts/console-parallelization',
+                'conflict' => [
+                    'symfony/service-contracts' => '<2.0',
+                    'symfony/cache' => '>=5.0',
+                    'symfony/framework-bundle' => '>=5.0',
+                ],
+            ]),
+            ['symfony/cache', 'symfony/framework-bundle'],
+            null,
+            self::toStdClass([
+                'package' => 'webmozarts/console-parallelization',
+                'conflict' => [
+                    'symfony/service-contracts' => '<2.0',
+                ],
+            ]),
+        ];
+    }
+
+    private static function toStdClass(array $value): stdClass
+    {
+        foreach ($value as $key => $item) {
+            if (is_array($item)) {
+                $value[$key] = self::toStdClass($item);
+            }
+        }
+
+        return (object) $value;
+    }
+}
