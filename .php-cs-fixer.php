8114fe1a8ea8154432e1315213e651be4a267e55
Théo FIDRY <theo.fidry@webmozarts.com>
2022-10-19 23:57:58 +0200

    Fix

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -18,7 +18,6 @@ $finder = Finder::create()
         '.github/fix-symfony-versions',
         'set-composer-conflicts.php',
     ])
-    ->notPath('tests/ErrorHandler/Container/LegacyResettableContainer.php')
     ->exclude([
         'dist',
         'tests/Integration/var',
