333eba7fcbdd03f14e217325cd24890ec447d027
Théo FIDRY <theo.fidry@webmozarts.com>
2022-10-28 00:02:09 +0200

    Fix PHPUnit coverage

--- src/Configuration.php
+++ src/Configuration.php
@@ -99,6 +99,8 @@ final class Configuration
     }
 
     /**
+     * @codeCoverageIgnore
+     *
      * @return positive-int
      */
     public function getNumberOfProcesses(): int
@@ -107,6 +109,8 @@ final class Configuration
     }
 
     /**
+     * @codeCoverageIgnore
+     *
      * @return positive-int
      */
     public function getSegmentSize(): int
@@ -115,6 +119,8 @@ final class Configuration
     }
 
     /**
+     * @codeCoverageIgnore
+     *
      * @return positive-int|null
      */
     public function getNumberOfSegments(): ?int
@@ -123,6 +129,8 @@ final class Configuration
     }
 
     /**
+     * @codeCoverageIgnore
+     *
      * @return positive-int|0|null
      */
     public function getTotalNumberOfBatches(): ?int
@@ -200,23 +208,19 @@ final class Configuration
     }
 
     /**
-     * @param 0|positive-int|null $numberOfItems
-     * @param positive-int        $segmentSize
-     * @param positive-int        $batchSize
-     * @param positive-int        $numberOfSegments
+     * @param 0|positive-int $numberOfItems
+     * @param positive-int   $segmentSize
+     * @param positive-int   $batchSize
+     * @param positive-int   $numberOfSegments
      *
-     * @return 0|positive-int|null
+     * @return 0|positive-int
      */
     private static function calculateTotalNumberOfBatches(
-        ?int $numberOfItems,
+        int $numberOfItems,
         int $segmentSize,
         int $batchSize,
         int $numberOfSegments
-    ): ?int {
-        if (null === $numberOfItems) {
-            return null;
-        }
-
+    ): int {
         if ($numberOfSegments >= 2) {
             // It "should" be `$numberOfSegments - 1`. However, it actually does
             // not matter as the expression L128 is just going to give a
