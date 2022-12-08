3a23cbf3d244128a995a1250646c9ecf7457549a
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2022-12-08 10:50:35 +0100

    Add test for CpuCoreCounter (#192)

---
+++ tests/PHPUnit/ResetCpuCounterListener.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Webmozarts Console Parallelization package.
+ *
+ * (c) Webmozarts GmbH <office@webmozarts.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Webmozarts\Console\Parallelization\PHPUnit;
+
+use PHPUnit\Runner\AfterTestHook;
+use ReflectionClass;
+use ReflectionProperty;
+use Webmozarts\Console\Parallelization\CpuCoreCounter;
+
+final class ResetCpuCounterListener implements AfterTestHook
+{
+    private static ?ReflectionProperty $countReflection;
+
+    public function executeAfterTest(string $test, float $time): void
+    {
+        self::resetCounter();
+    }
+
+    private static function getCountReflection(): ReflectionProperty
+    {
+        if (!isset(self::$countReflection)) {
+            self::$countReflection = (new ReflectionClass(CpuCoreCounter::class))->getProperty('count');
+            self::$countReflection->setAccessible(true);
+        }
+
+        return self::$countReflection;
+    }
+
+    private static function resetCounter(): void
+    {
+        $countReflection = self::getCountReflection();
+        $countReflection->setValue(new CpuCoreCounter(), null);
+    }
+}
